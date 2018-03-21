7224f8f0a41f388dcc201d9b3c2b7b6866349f2c
v.churkin <v.churkin@room8studio.com>
2018-03-21 18:23:36 +0200

    - transpiling of es6 extension added

--- app.js
+++ app.js
@@ -2,8 +2,8 @@
 // webpack ./fileName.js bundle.js  
 // or if you have webpack configuration file, just execute in terminal:
 // webpack
-require("./login");
+require("./login.es6");
 
-document.write("Welcome, Watch !!! time");
+document.write("Welcome,  to Big Hair Concerts!!");
 
 console.log("App loaded");
--- webpack.config.js
+++ webpack.config.js
@@ -3,5 +3,24 @@ module.exports = {
     output: {
         filename: "bundle.js"
     },
-    watch: true
+    watch: true,
+    module: {
+        preloaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "jshint-loader"
+            }
+        ],
+        loaders: [
+            {
+                test: /\.es6$/,
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            }
+        ]
+    },
+    resolve: {
+        extentions: ["", ".js", ".es6"]
+    }
 }
